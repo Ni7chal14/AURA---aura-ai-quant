b3eb61eeed6932120816725611c1476a31c0d2ec
nischal1409@gmail.com <yourgithubemail@example.com>
2026-08-22 21:13:13 +0530

    Fix Streamlit package path

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+import sys
+from pathlib import Path
+
+sys.path.insert(
+    0,
+    str(Path(__file__).resolve().parent / "src"),
+)
 import streamlit as st
 import pandas as pd
 import plotly.express as px
